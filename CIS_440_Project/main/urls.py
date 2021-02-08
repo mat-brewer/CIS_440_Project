96662c1c8bc49d62ec701aebe74931b88a880d78
zt <zt@DESKTOP-NQRICDB>
2021-02-08 14:24:44 -0600

    Got the login tab to display onto a different page

--- CIS_440_Project/main/urls.py
+++ CIS_440_Project/main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 # the path name is 'home' and the url path is http://127.0.0.1:8000/
@@ -8,7 +9,9 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('about/', views.about, name='about'),
     path('order/', views.order, name='order'),
-    path('reviews/', views.reviews, name='reviews')
+    path('reviews/', views.reviews, name='reviews'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login')
     
+   
    
 ]
